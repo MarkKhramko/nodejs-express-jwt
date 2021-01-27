57be1177ad63d402957e0f8eab856a9a0defc140
Mark Khramko <markkhramko@gmail.com>
2021-01-27 23:52:01 +0100

    Add new response services to APIController and HomePageController

--- app/controllers/api/APIController.js
+++ app/controllers/api/APIController.js
@@ -1,10 +1,53 @@
+// Reponse protocols.
+const { 
+	createOKResponse,
+	createErrorResponse
+} = require('#services/responses/api');
+
+const _processError = (error, req, res) => {
+	// Default error message.
+	let errorMessage = error?.message ?? 'Internal server error';
+	// Default HTTP status code.
+	let statusCode = 500;
+
+	// Perform your process here...
+
+	return createErrorResponse({
+		res, 
+		error: {
+			message: errorMessage
+		},
+		status: statusCode
+	});
+};
+
 const APIController = () => {
-	const getStatus = (req, res) => {
-		return res.status(200).json({ message: 'API is fully functional!' });
+
+	const _getStatus = (req, res) => {
+		try{
+			// Try making some faulty operation here,
+			// to see how error will be displayed:
+			
+			// Like this TypeError.
+			// ({}).test();
+
+			// Otherwise it will successfully send operational status.
+			return createOKResponse({
+				res,
+				content:{
+					operational: true,
+					message: 'API is fully functional!'
+				}
+			});
+		}
+		catch(error){
+			console.error("APIController._getStatus error: ", error);
+			return _processError(error, req, res);
+		}
 	};
 
 	return {
-		getStatus
+		getStatus: _getStatus
 	};
 };
 
--- app/controllers/web/HomePageController.js
+++ app/controllers/web/HomePageController.js
@@ -1,11 +1,31 @@
+// Reponse protocols.
+const {
+	createOKResponse,
+	createErrorResponse
+} = require('#services/responses/web');
+
 const HomePageController = () => {
 
-	const getHomePage = (req, res) =>{
-		return res.render('home');
+	const _getHomePage = (req, res) =>{
+		try{
+			// Try making some faulty operation here,
+			// to see how error will be displayed:
+			
+			// Like this TypeError.
+			// ({}).test();
+
+			// Otherwise it will successfully render home page.
+			return createOKResponse(res, 'home');
+		}
+		catch(error){
+			console.error("HomePageController._getHomePage error:", error);
+			return createErrorResponse(res, error);
+		}
+		
 	};
 
 	return {
-		getHomePage
+		getHomePage: _getHomePage
 	};
 };
 
