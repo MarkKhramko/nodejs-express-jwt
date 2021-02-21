7a3a7722fa051880d7d79fbd51de98ddba8b3721
Mark Khramko <markkhramko@gmail.com>
2021-02-21 05:26:46 +0100

    Refactor JWT service

--- app/services/jwt.service.js
+++ app/services/jwt.service.js
@@ -1,16 +1,37 @@
 const jwt = require('jsonwebtoken');
-const Config = require('#configs/jwt');
+const { 
+	accessToken,
+	refreshToken
+} = require('#configs/jwt');
+const { addSeconds } = require('#utils/dates');
 
-const { secret, expiresIn } = Config;
 
 module.exports = {
-	issue: (payload) => jwt.sign(payload, secret, { expiresIn }),
-	verify: async (token) => {
-		jwt.verify(token, secret, {}, (err, parsedToken)=>{
-			if (!!err)
-				return Promise.reject(err);
-
-			return Promise.resolve(parsedToken);
-		});
-	}
+	issueAccessToken: (payload) => _issueToken({ payload, secret:accessToken.secret, expiresIn:accessToken.expiresIn }),
+	issueRefreshToken: (payload) => _issueToken({ payload, secret:refreshToken.secret, expiresIn:refreshToken.expiresIn }),
+	verifyAccessToken: (token) => _verifyToken({ token, secret:accessToken.secret }),
+	verifyRefreshToken: (token) => _verifyToken({ token, secret:refreshToken.secret })
 };
+
+async function _issueToken({ payload, secret, expiresIn }) {
+	try {
+		const token = jwt.sign(payload, secret, { expiresIn });
+		const expirationDateValue = (addSeconds(new Date(), expiresIn/1000)).valueOf();
+
+		const fullToken = { token, expiresIn, expirationDateValue };
+		return Promise.resolve([ fullToken ]);
+	}
+	catch(error) {
+		return Promise.reject(error);
+	}
+}
+
+async function _verifyToken({ token, secret }) {
+	try {
+		const parsedToken = await jwt.verify(token, secret, {});
+		return Promise.resolve([ parsedToken ]);
+	}
+	catch(error) {
+		return Promise.reject(error);
+	}
+}
