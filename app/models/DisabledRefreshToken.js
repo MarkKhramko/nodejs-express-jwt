116d6b4fc874276afa295ab2c339babbd3bc58e8
Mark Khramko <markkhramko@gmail.com>
2021-02-21 05:20:47 +0100

    Add DisabledRefreshToken model for blocked JWT

---
+++ app/models/DisabledRefreshToken.js
@@ -0,0 +1,71 @@
+// ORM:
+const { DataTypes } = require('sequelize');
+const database = require('#services/db.service');
+
+// Custom error.
+const { Err } = require('#factories/errors');
+
+
+const DisabledRefreshToken = database.define(
+	'DisabledRefreshToken',
+	{
+		token: {
+			type: DataTypes.STRING,
+			required: true,
+			allowNull: false,
+			unique: true
+		},
+		UserId: {
+			type: DataTypes.INTEGER,
+			required: true,
+			allowNull: false
+		}
+	},
+	{
+		timestamps: true,
+		paranoid: true
+	}
+);
+
+// Static methods:
+DisabledRefreshToken.associate = models => {
+	models.DisabledRefreshToken.belongsTo(models.User, {
+		foreignKey: "UserId",
+		as: 'user'
+	});
+}
+
+DisabledRefreshToken.createOrFind = function({ token, userId }) {
+	const where = {
+		token
+	};
+
+	const defaults = {
+		token:token,
+		UserId:userId
+	};
+
+	const query = {
+		where,
+		defaults
+	};
+	return this.findOrCreate(query);
+}
+
+DisabledRefreshToken.selectAll = function({ token }) {
+	const where = {
+		token
+	};
+	const query = { where };
+	return this.findAll(query);
+}
+// Static methods\
+
+// Instance methods:
+DisabledRefreshToken.prototype.toJSON = function() {
+	const values = Object.assign({}, this.get());
+	return values;
+}
+// Instance methods\
+
+module.exports = DisabledRefreshToken;
