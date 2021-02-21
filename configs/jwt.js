41fbf569921f5692d1e62d44f6835e0ae98dd68c
Mark Khramko <markkhramko@gmail.com>
2021-02-21 04:53:22 +0100

    Add configs for JWT access and refresh tokens

--- configs/jwt.js
+++ configs/jwt.js
@@ -1,4 +1,13 @@
+const accessToken = {
+	secret: process.env.JWT_ACCESS_SECRET,
+	expiresIn: 10*60*1000 // 10 mins
+}
+const refreshToken = {
+	secret: process.env.JWT_REFRESH_SECRET,
+	expiresIn: 14*24*60*60*1000 // 14 days
+}
+
 module.exports = {
-	secret: process.env.JWT_SECRET || "secret",
-	expiresIn: 2 * 60 * 1000 // 2 minutes
+	accessToken,
+	refreshToken
 }
