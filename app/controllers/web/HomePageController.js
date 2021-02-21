5e35b9930d2fc81ba98ba3019364589db0f1e262
Mark Khramko <markkhramko@gmail.com>
2021-02-21 05:25:13 +0100

    Fix typos

--- app/controllers/web/HomePageController.js
+++ app/controllers/web/HomePageController.js
@@ -7,7 +7,7 @@ const {
 const HomePageController = () => {
 
 	const _getHomePage = (req, res) =>{
-		try{
+		try {
 			// Try making some faulty operation here,
 			// to see how error will be displayed:
 			
@@ -17,16 +17,15 @@ const HomePageController = () => {
 			// Otherwise it will successfully render home page.
 			return createOKResponse(res, 'home');
 		}
-		catch(error){
+		catch(error) {
 			console.error("HomePageController._getHomePage error:", error);
 			return createErrorResponse(res, error);
 		}
-		
-	};
+	}
 
 	return {
 		getHomePage: _getHomePage
-	};
-};
+	}
+}
 
 module.exports = HomePageController;
