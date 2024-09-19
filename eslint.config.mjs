c1ea950ad64762b80ce31803617426d036f0dec3
Mark Khramko <markkhramko@gmail.com>
2024-09-19 12:15:22 +0200

    Add linter (eslint)

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+  	files: ["**/*.js"],
+  	languageOptions: {
+  		sourceType: "commonjs"
+  	}
+  },
+  
+  {
+  	languageOptions: {
+  		globals: globals.node
+  	}
+  },
+
+  pluginJs.configs.recommended,
+];
